4857d97ec01de97177809b90a8c2b44f28abb905
Oleg Blokhin <blohin.o.d@gmail.com>
2014-11-06 01:28:36 +0300

    Add left panel with faces

--- csharp_product/AveragePortrait/AP.Gui/MainWindowViewModel.cs
+++ csharp_product/AveragePortrait/AP.Gui/MainWindowViewModel.cs
@@ -1,4 +1,11 @@
-﻿using AP.Logic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls.Primitives;
+using AP.Logic;
+using Emgu.CV;
+using Emgu.CV.Structure;
 using Microsoft.TeamFoundation.MVVM;
 using System.Windows.Media.Imaging;
 using Emgu.CV.WPF;
@@ -9,11 +16,37 @@ namespace AP.Gui
     {
         public BitmapSource AverageFaceResult { get; set; }
 
+        public IList<BitmapSource> Thumbnails { get; set; }
+
         public void MakeAveragePortrait()
         {
-            var image = Program.MakeAveragePortrait();
-            AverageFaceResult = BitmapSourceConvert.ToBitmapSource(image);
+            Thumbnails = new List<BitmapSource>();
+            CreateAveragePortrait();
             RaisePropertyChanged("AverageFaceResult");
+            RaisePropertyChanged("Thumbnails");
+        }
+
+        private void CreateAveragePortrait(bool cropFace = false)
+        {
+            var averageFace = new AverageFace(600, 600);
+            var images = Directory.GetFiles("images", "*.jpg");
+            var faceProcessor = new FaceProcessor();
+            var standardEyes = new List<Eye>
+                {
+                    new Eye {X = 200, Y = 200},
+                    new Eye {X = 250, Y = 300}
+                };
+            var faces = images.Select(image => new Face(faceProcessor, image, standardEyes, cropFace)).ToList();
+            foreach (var face in faces)
+            {
+                averageFace.Add(face.FaceBitmap);
+            }
+            averageFace.MakeAverage();
+            AverageFaceResult = BitmapSourceConvert.ToBitmapSource(averageFace.Result);
+            foreach (var face in faces)
+            {
+                Thumbnails.Add(BitmapSourceConvert.ToBitmapSource(face.Thumbnail));
+            }
         }
     }
 }
---
+++ csharp_product/AveragePortrait/AP.Logic/Face.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace AP.Logic
+{
+    public class Face
+    {
+        public Face(FaceProcessor faceProcessor, string imagePath, List<Eye> standardEyes, bool cropFace)
+        {
+            Eyes = new List<Eye>();
+            OriginalBitmap = new Image<Bgr, byte>(imagePath);
+            Thumbnail = OriginalBitmap.Resize(200, 200, INTER.CV_INTER_LINEAR, preserveScale: true);
+
+            var faces = faceProcessor.GetFaces(OriginalBitmap);
+            var face = faces.Single();
+            if (cropFace)
+            {
+                FaceBitmap = faceProcessor.GetRectFromImage(OriginalBitmap, face);
+                face = Rectangle.Empty;
+            }
+            else
+            {
+                FaceBitmap = OriginalBitmap;
+            }
+            var eyes = faceProcessor.GetEyes(FaceBitmap, face);
+
+            foreach (var eye in eyes)
+            {
+                Eyes.Add(new Eye
+                {
+                    X = (float)(eye.X + eye.Width / 2.0),
+                    Y = (float)(eye.Y + eye.Height / 2.0)
+                });
+            }
+
+            foreach (var eye in Eyes)
+                FaceBitmap.Draw(new Rectangle((int)(eye.X - 10), (int)(eye.Y - 10), 20, 20), new Bgr(Color.Red), 2);
+
+            var transform = Transformation.Construct(Eyes, standardEyes);
+            var rotationMatrix = transform.AsMatrix<float>();
+            FaceBitmap = FaceBitmap.WarpAffine(rotationMatrix, 600, 600, INTER.CV_INTER_CUBIC, WARP.CV_WARP_DEFAULT, new Bgr(Color.Black));
+            var translationMatrix = new Matrix<float>(new float[,]
+                {
+                    {1, 0, transform.Translation.X},
+                    {0, 1, transform.Translation.Y}
+                });
+            FaceBitmap = FaceBitmap.WarpAffine(translationMatrix, 600, 600, INTER.CV_INTER_CUBIC, WARP.CV_WARP_DEFAULT, new Bgr(Color.Black));
+            FaceBitmap = faceProcessor.IncreaseImageSize(FaceBitmap, 600, 600);
+
+        }
+
+        public Image<Bgr, byte> OriginalBitmap { get; set; }
+        public Image<Bgr, byte> Thumbnail { get; set; }
+        public Image<Bgr, byte> FaceBitmap { get; set; }
+        public List<Eye> Eyes { get; set; }
+    }
+}
